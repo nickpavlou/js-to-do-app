847bc2b98c6495bcb671e29fa4d418e534deaae8
nick_pavlou <nickpavlou@mail.com>
2023-06-03 23:56:06 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,113 @@
+sconst newTaskInput = document.querySelector('#new-task input');
+const tasksDiv = document.querySelector('#tasks');
+let deleteTasks, editTasks, task;
+let updateNote = '';
+let count;
+
+//Function on window load
+window.onload = () => {
+  updateNote = '';
+  count = Object.keys(localStorage).length;
+  displayTasks;
+}
+
+// Function to Display the Tasks
+const displayTasks = () => {
+  if (Object.keys(localStorage).length > 0) {
+    tasksDiv.style.display = 'inline-block';
+  } else {
+    tasksDiv.style.display = 'none';
+  }
+
+  Clear the tasks
+  tasksDiv.innerHTML = '';
+
+  // Get all values
+  let tasks = Objects.keys(localStorage);
+  tasks = tasks.sort();
+
+  for (let key of tasks) {
+    let classValue = '';
+
+    // Get all values
+    let value = localStorage.getItem(key);
+    let taskInnerDiv = document.createElement('div');
+    taskInnerDiv.classList.add('task');
+    taskInnerDiv.setAttribute('id', key);
+    taskInnerDiv.innerHTML = `<span id="taskname">${key.split('_')[1]}</span>`;
+
+    // local storage would store boolea as string so we parse it to boolean back
+    let editButton = document.createElement('button');
+    editButton.classList.add('edit');
+    editButton.innerHTML = `<i class="fa-solid fa-pen-to-square"></i>`;
+    if(!JSON.parse(value)) {
+      editButton.style.visibility = 'visible'
+    } else {
+      editButton.style.visibility = 'hidden';
+      taskInnerDiv.classList.add('completed');
+    }
+    taskInnerDiv.appendChild(editButton);
+    taskInnerDiv.innerHTML += `<button class="delete"><i class="fa-solid fa-trash"></i></button>`;
+    tasksDiv.appendChild(taskInnerDiv);
+  }
+
+  // tasks completed
+  tasks = document.querySelectorAll('.task');
+  tasks.forEach((element, index) => {
+    element.onclick = () => {
+      //local storage update
+      if (element.classList.contains('completed')) {
+        updateStorage(element.id.split('_'[0], element.innerText, false))
+      }
+      else {
+        updateStorage(element.id.split('_')[0],
+        element.innerText, true)
+      }
+    }
+  })
+  // Edit
+}
+
+// Disable edit button
+const disableButtons = (bool) => {
+  let editButton = document.getElementsByClassName('edit');
+  Array.from(editButtons).forEach((element) => {
+    element.disabled = bool;
+  });
+};
+
+// Remove task from local storage
+const removeTask = (taskValue) => {
+  localStorage.removeItem(taskValue);
+  displayTasks();
+}
+
+// Add to local storage
+const updateStorage = (index, taskValue, completed) => {
+  localStorage.setItem(`${index}_${taskValue}`, completed);
+  displayTasks();
+}
+
+// Function to add new task
+document.querySelector('#push').addEventListener('click', () => {
+  //Enable the edit button
+  disableButtons(false);
+if(newTaskInput.value.length === 0) {
+  alert('Please enter a task');
+} else {
+  // Store locally and display from local storage
+  if (updateNote === '') {
+    // new task  
+    updateStorage(count, newTaskInput.value, false); 
+} else {
+  // update task
+  let existingCount = updateNote.split('_')[0];
+  removeTask(updateNote);
+  updateStorage(existingCount, newTaskInput.value, false);
+  updateNote = ''
+}
+count += 1;
+newTaskInput.value = '';
+}
+
+// https://youtu.be/O0YQBdpOv5A?t=1320
